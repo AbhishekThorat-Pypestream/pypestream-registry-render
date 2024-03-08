cf7c078c1091ba5e4929055111ee1655f5e0eded
Abhishek Thorat <abhishek.thorat18@gmail.com>
2024-03-08 20:09:00 +0530

    - Basic verdaccio registry app

---
+++ .eslintrc.cjs
@@ -0,0 +1,5 @@
+module.exports = {
+  extends: [
+    '@pypestream/eslint-config'
+  ]
+};
---
+++ vite.config.ts
@@ -0,0 +1,5 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({});
